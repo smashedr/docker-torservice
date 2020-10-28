8fc0307ed02b39db1ee7af863226165c8c576929
Shane <shane@shanerice.me>
2020-10-27 20:41:50 -0700

    Docker image time

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+sed -i "s/TOR_PORT/${TOR_PORT}/" /etc/tor/torrc
+
+echo "Set tor to listen on port: ${TOR_PORT}"
+
 ls -lah /tor
 
-(sleep 15 && printf "\n\n***Hostname: " && cat /tor/hostname) &
+(sleep 5;while true;do until [ -f /tor/hostname ];do sleep 5;done;echo;cat /tor/hostname;echo;break;done) &
 
 exec "tor"
